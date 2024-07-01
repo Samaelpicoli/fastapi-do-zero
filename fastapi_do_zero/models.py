aba8dc75c9138b467707631108dbae01b5f267e0
Samael Picoli <samaelmunizpicoli27@gmail.com>
2024-07-01 18:33:01 -0300

    correcao alembic

--- fastapi_do_zero/models.py
+++ fastapi_do_zero/models.py
@@ -38,6 +38,7 @@ class User:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+    # Exercício aula 4
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), onupdate=func.now()
     )
